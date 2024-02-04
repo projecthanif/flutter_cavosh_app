5069df4d62738f96083e3266f6d52366b570f0a0
projecthanif <iamustapha213@gmail.com>
2024-02-04 06:41:40 +0100

    arranging the setup

---
+++ lib/misc/color.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color button1 = Color.fromRGBO(52, 83, 106, 1);
+  static const Color txt = Color.fromRGBO(90, 126, 153, 1);
+  static const Color two = Color.fromRGBO(230, 87, 53, 1);
+  static const Color three = Color.fromRGBO(254, 228, 214, 1);
+  static const Color four = Color.fromRGBO(41, 52, 65, 1);
+}
